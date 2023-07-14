b78fb14ead25540d8bac82cf3ce5d11efd81da7c
anasaqeeel <122775288+anasaqeeel@users.noreply.github.com>
2023-07-15 01:49:21 +0500

    q5

---
+++ q5/q5.ts
@@ -0,0 +1,6 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person:string="Steve Jobs";
+let msg:string=famous_person+" once said, "+'"Stay Hungary, Stay Foolish!"';
+console.log(msg);
+export{};
