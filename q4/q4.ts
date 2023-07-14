57dbff752820f1733ee008300a2fd04509cf52e8
anasaqeeel <122775288+anasaqeeel@users.noreply.github.com>
2023-07-15 01:46:03 +0500

    Update q4.ts

--- q4/q4.ts
+++ q4/q4.ts
@@ -1,3 +1,8 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
 let author:string="Steve Jobs";
 let msg:string='"Stay Hungary, Stay Foolish!"';
 console.log(author+" once said, "+msg);
