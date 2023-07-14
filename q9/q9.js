a35cac168ec13155cda3ef9886f14c599ebb37a5
anasaqeeel <122775288+anasaqeeel@users.noreply.github.com>
2023-07-15 02:11:35 +0500

    q9

---
+++ q9/q9.js
@@ -0,0 +1,4 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var x = 18;
+var msd = "my fav number is >> :" + x;
+console.log(msd);
---
+++ q9/q9.ts
@@ -0,0 +1,4 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let x:number=18;
+let msd:string="my fav number is >> :"+x;
+console.log(msd);
